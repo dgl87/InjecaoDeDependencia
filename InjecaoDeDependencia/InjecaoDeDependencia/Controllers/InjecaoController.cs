f60e179779001314c6404d13f80a30b8d813846b
Douglas <dglandre@gmail.com>
2022-10-02 14:02:57 -0300

    Configurando e Utilizando Injecao via Construtor, Anotacao e Provider

---
+++ InjecaoDeDependencia/InjecaoDeDependencia/Controllers/InjecaoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace InjecaoDeDependencia.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InjecaoController : ControllerBase
+    {
+        private IOperacao _operacao;
+        private IServiceProvider _provider;
+        public InjecaoController(IOperacao operacao, IServiceProvider provider)
+        {
+            _operacao = operacao;
+            _provider = provider;
+        }
+
+        [HttpGet("Construtor")]
+        public IActionResult Construtor()
+        {
+            return Ok(_operacao.Id);
+        }
+
+        [HttpGet("Anotacao")]
+        public IActionResult Anotacao([FromServices] IOperacao operacao)
+        {
+            return Ok(operacao.Id);
+        }
+
+        [HttpGet("Provider")]
+        public IActionResult Provider()
+        {
+            return Ok(_provider.GetRequiredService<IOperacao>());
+        }
+    }
+}
---
+++ InjecaoDeDependencia/InjecaoDeDependencia/Operacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InjecaoDeDependencia
+{
+    public class Operacao : IOperacao
+    {
+        public Guid Id { get; set; }
+        public Operacao()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+
+    public interface IOperacao
+    {
+        Guid Id { get; set; }
+    }
+}
